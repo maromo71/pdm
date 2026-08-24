4597ea45f0b679c6be3d150df394a367c1f1506d
Maromo <professormoraes@gmail.com>
2026-08-24 11:01:22 -0300

    Aula do dia 21 agosto - finalizado exerc 10

--- aula2108/js/script10.js
+++ aula2108/js/script10.js
@@ -1,10 +1,18 @@
 let inputNum1 = document.getElementById("txtNum1")
 let inputNum2 = document.getElementById("txtNum2")
 let buttonSomar = document.getElementById("btnSomar")
-
+let p1 = document.getElementById("p1")
 
 function somar(x, y){
     let result = x + y
     alert(result)
+    //podemos apresentar o resultado no html
+    p1.innerHTML = "Resultado: " + result
 }
-buttonSomar.addEventListener("click",somar(12, 5))
+buttonSomar.addEventListener("click",function(ev){
+    ev.preventDefault() //Prevenir atualizacao
+    let n1 = Number(inputNum1.value)
+    let n2 = Number(inputNum2.value)
+
+    somar(n1, n2)
+})
